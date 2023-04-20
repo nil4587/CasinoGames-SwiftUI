11a65c423634131c87fd1ae9486d43594479408d
NileshKumar Prajapati <3828906+nil4587@users.noreply.github.com>
2023-04-20 13:15:29 +0200

    Initial Commit

---
+++ SwiftUIGame/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIGame
+//
+//  Created by Nileshkumar M. Prajapati on 2023/04/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIGame/SwiftUIGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGameApp.swift
+//  SwiftUIGame
+//
+//  Created by Nileshkumar M. Prajapati on 2023/04/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
